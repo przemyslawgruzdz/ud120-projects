15250d5c5e85f7f71b1f5e110d289424dc29fb12
Przemyslaw Gruzdz <przemyslaw.gruzdz@gmail.com>
2019-11-03 14:25:41 +0100

    After L6

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -32,7 +32,22 @@ plt.show()
 ### visualization code (prettyPicture) to show you the decision boundary
 
 
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
+from time import time
+import collections, numpy
+clf = AdaBoostClassifier(n_estimators=100, random_state=0)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+print accuracy_score(pred, labels_test)
+print collections.Counter(pred)
 
 
 
--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,7 +24,28 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from time import time
+import collections, numpy
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
 
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+print len(features_train[0])
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+print accuracy_score(pred, labels_test)
+print pred[10] ,  "," ,  pred[26] ,  "," , pred[50]
+print collections.Counter(pred)
 
 #########################################################
 
