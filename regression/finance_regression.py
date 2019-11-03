052b3a86428f189c35293f25c8bb031154cc6a85
Przemyslaw Gruzdz <przemyslaw.gruzdz@gmail.com>
2019-11-03 20:39:58 +0100

    After L7

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -26,10 +26,10 @@ data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
 ### training-testing split needed in regression, just like classification
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,10 +38,15 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+print reg.coef_
+print reg.intercept_
 
-
-
-
+from sklearn.metrics import r2_score
+print reg.score(feature_train, target_train)
+print reg.score(feature_test, target_test)
 
 
 
