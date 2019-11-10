78e85480510c90421f38c95305f7384fa6615e1a
Przemyslaw Gruzdz <przemyslaw.gruzdz@gmail.com>
2019-11-10 15:14:06 +0100

    After L10

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -44,6 +44,7 @@ data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r")
 data_dict.pop("TOTAL", 0)
 
 
+
 # the features to be used
 features_list = ['poi', 'salary', 'exercised_stock_options']
 
@@ -80,3 +81,23 @@ print df.loc['exercised_stock_options',:].min(skipna=True)
 df.loc['salary',:] = pd.to_numeric(df.loc['salary',:], errors='coerce')
 print df.loc['salary',:].max(skipna=True)
 print df.loc['salary',:].min(skipna=True)
+
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+
+features_list = ['poi', 'salary', 'exercised_stock_options']
+
+data = featureFormat(data_dict, features_list)
+_, salary, stock = zip(*data)
+
+# put the features into 2-D numpy arrays
+salary = np.array(salary).reshape((len(salary),1))
+stock = np.array(stock).reshape((len(stock),1))
+
+# rescale
+scaler = MinMaxScaler()
+salary = scaler.fit_transform(salary)
+print '$200,000 becomes {0}'.format(scaler.transform([[200000.]])[0][0])
+
+stock = scaler.fit_transform(stock)
+print '$1,000,000 becomes {0}'.format(scaler.transform([[1000000.]])[0][0])
