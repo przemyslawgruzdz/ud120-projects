91036eef76f59e6010b980b589206e7bba8f3909
Przemyslaw Gruzdz <przemyslaw.gruzdz@gmail.com>
2019-12-28 22:37:45 +0100

    after L13

--- pca/eigenfaces.py
+++ pca/eigenfaces.py
@@ -23,12 +23,12 @@ import logging
 import pylab as pl
 import numpy as np
 
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_lfw_people
-from sklearn.grid_search import GridSearchCV
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix
-from sklearn.decomposition import RandomizedPCA
+from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 
 # Display progress logs on stdout
@@ -70,7 +70,7 @@ n_components = 150
 
 print "Extracting the top %d eigenfaces from %d faces" % (n_components, X_train.shape[0])
 t0 = time()
-pca = RandomizedPCA(n_components=n_components, whiten=True).fit(X_train)
+pca = PCA(n_components=n_components, svd_solver='randomized', whiten=True).fit(X_train)
 print "done in %0.3fs" % (time() - t0)
 
 eigenfaces = pca.components_.reshape((n_components, h, w))
@@ -144,3 +144,32 @@ eigenface_titles = ["eigenface %d" % i for i in range(eigenfaces.shape[0])]
 plot_gallery(eigenfaces, eigenface_titles, h, w)
 
 pl.show()
+
+print 'Variance explained by the first principal component:  {0}'.format(pca.explained_variance_ratio_[0])
+print 'Variance explained by the second principal component: {0}'.format(pca.explained_variance_ratio_[1])
+
+for n_components in [10, 15, 25, 50, 100, 250]:
+  pca = PCA(n_components=n_components, svd_solver='randomized', whiten=True).fit(X_train)
+
+  eigenfaces = pca.components_.reshape((n_components, h, w))
+
+  X_train_pca = pca.transform(X_train)
+  X_test_pca = pca.transform(X_test)
+
+
+  # Train a SVM classification model
+  param_grid = {
+           'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+            'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
+            }
+  clf = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+  clf = clf.fit(X_train_pca, y_train)
+
+
+  # Quantitative evaluation of the model quality on the test set
+  y_pred = clf.predict(X_test_pca)
+
+  if n_components==10:
+    print 'n_components' + classification_report(y_test, y_pred, target_names=target_names).split('\n')[0]
+
+  print '{0:12d}'.format(n_components) + classification_report(y_test, y_pred, target_names=target_names).split('\n')[2]
