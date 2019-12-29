2c95b9eaaa89261ad9c2cac8214654f61e1f80ff
Przemyslaw Gruzdz <przemyslaw.gruzdz@gmail.com>
2019-12-29 19:58:34 +0100

    after L15

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -28,4 +28,41 @@ labels, features = targetFeatureSplit(data)
 
 ### your code goes here 
 
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, precision_score, classification_report, recall_score
 
+clf = DecisionTreeClassifier()
+clf.fit(features,labels)
+
+print clf.score(features,labels)
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
+
+pred = clf.predict(features_test)
+print confusion_matrix(labels_test, pred)
+
+print precision_score(labels_test, pred)
+
+print classification_report(labels_test, pred)
+
+print recall_score(labels_test, pred)
+
+
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1] 
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+
+cm = confusion_matrix(true_labels, predictions)
+
+print cm, '\n'
+print '{0} True positives'.format(cm[1][1])
+print '{0} True negatives'.format(cm[0][0])
+print '{0} False positives'.format(cm[0][1])
+print '{0} False negatives'.format(cm[1][0])
+
+print precision_score(true_labels, predictions)
+
+print recall_score(true_labels, predictions)
